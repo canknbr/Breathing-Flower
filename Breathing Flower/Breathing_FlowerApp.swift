c3b822baf818382350adcb718d644e8059b83ac3
canknbr <cankanbur1@gmail.com>
2023-04-03 13:43:50 +0300

    Initial Commit

---
+++ Breathing Flower/Breathing_FlowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Breathing_FlowerApp.swift
+//  Breathing Flower
+//
+//  Created by Can Kanbur on 3.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Breathing_FlowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
